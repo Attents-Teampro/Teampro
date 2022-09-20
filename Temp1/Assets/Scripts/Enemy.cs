1f7170d79edbaec9f9a3f0148b25d05fbe97d050
parkinyeop <parkinyeop@naver.com>
2022-09-20 14:48:17 +0900

    유니티 폴더 이동

--- Temp1/Assets/Scripts/Enemy.cs
+++ Temp1/Assets/Scripts/Enemy.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,12 +12,16 @@ public class Enemy : MonoBehaviour
     public int curHealth;
     public int maxHealth;
     public Transform target;            //플레이어 타겟
+    public float moveSpeed = default;             //이동 속도
+    public float rotSpeed = 1.0f;
+
+    Vector3 targetDirection;
 
     public GameObject projectile;       //원거리용 발사체
     public BoxCollider meleeAttack;     //밀리 어택용 컬리젼 박스
     public NavMeshAgent nav;            //네비 매쉬를 사용
-    public Rigidbody rigid;
-    public BoxCollider boxCollider;     //피격에 사용되는 기본 컬리젼
+    public Rigidbody rb;
+    public CapsuleCollider capsuleCollider;     //피격에 사용되는 기본 컬리젼
     public Animator anim;
 
     public bool isChase;                //타겟을 향해 이동중
@@ -30,29 +36,47 @@ public class Enemy : MonoBehaviour
 
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
-        boxCollider = GetComponent<BoxCollider>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        meleeAttack = GetComponent<BoxCollider>();
+        target = target.GetComponent<Transform>();
     }
 
     private void Start()
     {
         //meleeAttack.enabled = false;        //밀리 어택 컬리젼을 꺼 둠
         //StartCoroutine(Action());           //코루틴 테스트용 임시 코드
+
     }
 
     private void Update()
+    {
+        AnimationTest();                     // 애니메이션 테스트 메소드 1,2,3,4
+        MoveToTarget();                     // 타겟을 향이 이동하는 메소드
+    }
+
+    private void MoveToTarget()
+    {
+        targetDirection = (target.position - transform.position).normalized;                //타겟 위치의 방향
+        rb.MovePosition(transform.position + targetDirection * Time.deltaTime * moveSpeed); //리지드바디를 사용하여 타겟으로 이동
+        transform.LookAt(target);                                                           //Lookat을 사용하여 타겟 바라보기
+        isChase = true;                                                                     // 이동중임을 알리는 bool 값
+        anim.SetBool("isWalk", true);                                                       // walk 애니메이션 활성화
+    }
+
+    private void AnimationTest()
     {
         if (Input.GetKeyDown("1") && !isAttackTest)
             StartCoroutine(Action1());
 
         if (Input.GetKeyDown("2") && !isWalkTest)
-            StartCoroutine(Action2()); 
-        
+            StartCoroutine(Action2());
+
         if (Input.GetKeyDown("3") && !isGetHitTest)
             StartCoroutine(Action3());
-        
+
         if (Input.GetKeyDown("4") && !isDieTest)
             StartCoroutine(Action4());
     }
@@ -96,16 +120,5 @@ public class Enemy : MonoBehaviour
         isDieTest = false;
         StopCoroutine(Action4());
     }
-    //IEnumerator Action()
-    //{
-    //    meleeAttack.enabled = true;         //밀리어택의 컬리젼을 켜 줌
-    //    anim.SetBool("isAttack", true);
-    //    int actionNum = Random.Range(0, 4);
-    //    switch (actionNum)
-    //    {
-    //        case 0:
-    //            anim.SetBool("isAttack", true);
-    //            break;
-    //    }
-    //}
+    
 }

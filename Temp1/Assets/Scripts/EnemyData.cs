c3ed4357a95d7782484041b7b5946d13566123ed
Inyeop Park <111333333+parkinyeop@users.noreply.github.com>
2022-10-15 17:30:23 +0900

    Enemy Data + Enemy Class 추가
    몬스터별 4종씩

---
+++ Temp1/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Enemy Data",menuName = "Scriptable Object/Zombie Data",order = int.MaxValue)]
+public class EnemyData : ScriptableObject
+{
+    [SerializeField]
+    string enemyType;
+    public string EnemyType { get { return enemyType; }}
+
+    [SerializeField]
+    int eHP;
+    public int EHP { get { return eHP; }}
+
+    [SerializeField]
+    int damage;
+    public int Damage { get { return damage; }}
+
+    [SerializeField]
+    float moveSpeed;
+    public float MoveSpeed { get { return moveSpeed; }}
+
+    [SerializeField]
+    float rotationSpeed;
+    public float RotationSpeed { get { return rotationSpeed; }}
+
+    [SerializeField]
+    float targetRadius;
+    public float TargetRadius { get { return targetRadius; }}
+
+    [SerializeField]
+    float targetRange;
+    public float TargetRange { get { return targetRange; }}
+}

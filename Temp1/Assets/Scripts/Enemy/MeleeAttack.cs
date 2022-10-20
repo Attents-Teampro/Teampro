bfd44f329c15ee1d4582254985af78ec7513cd62
parkinyeop <parkinyeop@naver.com>
2022-10-20 15:11:10 +0900

    Update MeleeAttack.cs
    공격자 이름 출력

--- Temp1/Assets/Scripts/Enemy/MeleeAttack.cs
+++ Temp1/Assets/Scripts/Enemy/MeleeAttack.cs
@@ -6,16 +6,27 @@ using static UnityEngine.GraphicsBuffer;
 public class MeleeAttack : MonoBehaviour, ICharacter
 {
     public EnemyData enemyData;
+    Enemy_Orc orc;
+    Enemy_Skelleton skelleton;
 
     private void Awake()
     {
         //enemyData = GetComponent<EnemyData>();
+        orc = GetComponentInParent<Enemy_Orc>();
+        skelleton = GetComponentInParent<Enemy_Skelleton>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("Attack");
+        {   
+            if(orc != null)
+            {
+            Debug.Log($"{orc.name}Attack");
+            }
+            else if(skelleton != null)
+            {
+                Debug.Log($"{skelleton.name}Attack");
+            }
             //Attack(enemy.target.gameObject,enemy.maxDamage);
             Attacked(enemyData.EDamage);
         }

6243f377e5e4182481e35eeb995929f237fec2f5
yhaynn <yhaynn@gmail.com>
2022-11-04 13:16:57 +0900

    HPbar
    Canvas, 2script

---
+++ Temp1/Assets/Scripts/UI/Health.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Player와 healthpreferences(UI 변경 스크립트)간의 값을 get set하는 스크립트
+//프로퍼티 개선 작업 필요
+public class Health : MonoBehaviour
+{
+    public static Health instance;
+
+    private HealthPreferences prefs;
+
+    
+    
+
+    void Start()
+    {
+        instance = this;
+        prefs = GetComponent<HealthPreferences>();
+        
+    }
+
+    public float GetCurrentHealth()
+    {
+        
+        return prefs.GetCurrentHealth();
+    }
+
+    public void SetCurrentHealth(float currentHealth)
+    {
+        
+        prefs.SetCurrentHealth(currentHealth);
+    }
+
+    public float GetTotalHealth()
+    {
+        return prefs.GetTotalHealth();
+    }
+
+    public void SetTotalHealth(float amount)
+    {
+        
+        prefs.SetTotalHealth(amount);
+    }
+
+    
+    //misc
+    //public void AddDamage(float damage)
+    //{    
+    //    prefs.AddDamage(damage);
+    //}
+
+    //public void AddHeal(float heal)
+    //{
+    //    prefs.AddHeal(heal);
+    //}
+
+    //public void SetImagesAmount(int amount)
+    //{
+    //    prefs.Init(amount);
+    //}
+
+    //public void AddImage()
+    //{
+    //    prefs.AddImage();
+    //}
+
+    //public void RemoveImage()
+    //{
+    //    prefs.RemoveImage();
+    //}
+
+    //public void SetFillType(Image.FillMethod type) {
+    //    prefs.SetFillType(type);
+    //}
+
+    //public void Reset()
+    //{
+    //    prefs.Reset();
+    //}
+}
---
+++ Temp1/Assets/Scripts/UI/HealthPreferences.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthPreferences : MonoBehaviour
+{
+    public GameObject fullHeartsContainer;  //채워진 하트만 있는 콘테이너
+    public GameObject emptyHeartsContainer; //빈 하트만 있는 콘테이너
+
+    public Sprite fullHeartSprite;          //채워진 하트의 스프라이트
+    public Sprite emptyHeartSprite;         //빈 하트의 스프라이트
+
+    public int imagesAmount;                //만들어져있는 하트 갯수
+
+    public float maxHealth;               //최대 체력
+    public float currentHealth;          //현재 체력
+
+    private float valuePerImage;            //하트 이미지 하나가 자긴 hp 밸류 (최대 hp값 / 하트 갯수.ex)최대hp 100,하트 갯수 4일땐 25, 5면 20)
+
+    Player player;
+    public Image.FillMethod fillMethod;     //이미지 채워지는 방식
+
+    //인스펙터창에서 가리기
+    [HideInInspector]
+    public Image.OriginHorizontal horizontalDirection;
+    [HideInInspector]
+    public Image.OriginVertical verticalDirection;
+    [HideInInspector]
+    public Image.Origin90 radial90Direction;
+    [HideInInspector]
+    public Image.Origin180 radial180Direction;
+    [HideInInspector]
+    public Image.Origin360 radial360Direction;
+
+    
+
+
+    private void Start()
+    {
+
+        player = Player.instance;
+        maxHealth = player.pMaxHp;
+        currentHealth = player.pHP;
+
+
+    }
+
+    private void OnValidate()
+    {
+        
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);  //현재체력은 0에서 base 값 사이를 벗어나지 않는다
+
+        if (gameObject.scene.IsValid()) //씬이 없다면?
+        {
+            if (!Application.isPlaying) //프로그램이 플레이중이면
+            {
+                RemoveAll();  //모두 제거
+#if UNITY_EDITOR
+                for (int i = 0; i < imagesAmount; i++)
+                    EditorApplication.delayCall += () => CreateImage(i); //하트 이미지를 생성한다
+#endif
+            }
+#if UNITY_EDITOR
+            EditorApplication.delayCall += () => UpdateHealth();
+#endif
+        }
+    }
+
+    
+
+
+    //플레이로 전환했을때 
+    private void RemoveAll() 
+    {
+        if (Application.isPlaying)
+        {
+            foreach(Transform child in fullHeartsContainer.transform)
+                Destroy(child.gameObject);
+
+            foreach (Transform child in emptyHeartsContainer.transform)
+                Destroy(child.gameObject);
+        }
+        else
+        {
+#if UNITY_EDITOR
+            foreach (Transform child in fullHeartsContainer.transform)
+                EditorApplication.delayCall += () => DestroyImmediate(child.gameObject);
+
+            foreach (Transform child in emptyHeartsContainer.transform)
+                EditorApplication.delayCall += () => DestroyImmediate(child.gameObject);
+#endif
+        }
+    }
+
+    /// <summary>
+    /// 이미지 생성
+    /// </summary>
+    public void Init(int amount) 
+    {
+        imagesAmount = amount;
+        //Debug.Log("init");
+        RemoveAll();
+
+        for (int i = 0; i < imagesAmount; i++)
+            CreateImage(i);
+    }
+
+    /// <summary>
+    /// 스프라이트 생성
+    /// </summary>
+    private void CreateImage(int index)
+    {
+        GameObject heartFull = new GameObject(); 
+        heartFull.tag = "Heart Full";
+        Image imgFull = heartFull.AddComponent<Image>();
+        imgFull.sprite = fullHeartSprite;
+        heartFull.GetComponent<RectTransform>().SetParent(fullHeartsContainer.transform);
+        heartFull.transform.localScale = Vector3.one;
+
+        imgFull.type = Image.Type.Filled;
+        imgFull.fillMethod = fillMethod;
+        switch (fillMethod)
+        {
+            case Image.FillMethod.Horizontal: imgFull.fillOrigin = (int)horizontalDirection; break;
+            case Image.FillMethod.Vertical: imgFull.fillOrigin = (int)verticalDirection; break;
+            case Image.FillMethod.Radial90: imgFull.fillOrigin = (int)radial90Direction; break;
+            case Image.FillMethod.Radial180: imgFull.fillOrigin = (int)radial180Direction; break;
+            case Image.FillMethod.Radial360: imgFull.fillOrigin = (int)radial360Direction; break;
+        }
+
+        valuePerImage = maxHealth / imagesAmount;
+
+        if ((index + 1) * valuePerImage > currentHealth) //
+        {
+            float temp = (index + 1) * valuePerImage - currentHealth;
+            float value = 1 - temp / valuePerImage;
+
+            imgFull.fillAmount = value;
+        }
+        else
+            imgFull.fillAmount = 1;
+
+        if (index * valuePerImage >= currentHealth)
+            imgFull.fillAmount = 0;
+
+
+        GameObject heartEmpty = new GameObject();
+        heartEmpty.tag = "Heart Empty";
+        Image imgEmpty = heartEmpty.AddComponent<Image>();
+        imgEmpty.sprite = emptyHeartSprite;
+        heartEmpty.GetComponent<RectTransform>().SetParent(emptyHeartsContainer.transform);
+        heartEmpty.transform.localScale = Vector3.one;
+    }
+
+    /// <summary>
+    /// 현재 체력만큼 스프라이트 이미지를 갱신하여 만든다
+    /// </summary>
+    private void UpdateHealth()
+    {
+        
+        valuePerImage = maxHealth / imagesAmount;
+        //이미지 당 값 =  기본 체력 /하트 갯수   =33.33 100/3
+        for (int i = 0; i < imagesAmount; i++)
+        {
+            if ((i + 1) * valuePerImage > currentHealth)
+            {
+                float temp = (i + 1) * valuePerImage - currentHealth;
+                float value = 1 - temp / valuePerImage;
+
+                fullHeartsContainer.transform.GetChild(i).GetComponent<Image>().fillAmount = value;
+            }
+            else
+                fullHeartsContainer.transform.GetChild(i).GetComponent<Image>().fillAmount = 1;
+
+            if (i * valuePerImage >= currentHealth)
+                fullHeartsContainer.transform.GetChild(i).GetComponent<Image>().fillAmount = 0;
+        }
+    }
+
+    
+    public void SetCurrentHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(amount, 0, maxHealth);
+        UpdateHealth();
+    }
+
+    
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+   
+    public float GetTotalHealth()
+    {
+        return maxHealth;
+    }
+
+    
+    public void SetTotalHealth(float amount) 
+    {
+        maxHealth = amount;
+        UpdateHealth();
+    }
+
+  
+    //misc
+    //public void AddDamage(float amount)
+    //{
+        
+    //        Debug.Log("HPref-add damage");
+    //        currentHealth -= amount;
+
+    //        if (currentHealth < 0)
+    //            currentHealth = 0;
+
+    //        UpdateHealth();
+        
+    //}
+
+    ///// <summary>
+    ///// Increase current health by amount
+    ///// </summary>
+    //public void AddHeal(float amount)
+    //{
+    //    currentHealth += amount;
+
+    //    if (currentHealth > maxHealth)
+    //        currentHealth = maxHealth;
+
+    //    UpdateHealth();
+    //}
+
+    ///// <summary>
+    ///// Add one image
+    ///// </summary>
+    //public void AddImage()
+    //{
+    //    imagesAmount += 1;
+    //    Init(imagesAmount);
+    //}
+
+    ///// <summary>
+    ///// Remove one image
+    ///// </summary>
+    //public void RemoveImage()
+    //{
+    //    if (imagesAmount > 0)
+    //    {
+    //        imagesAmount -= 1;
+    //        Init(imagesAmount);
+    //    }
+    //}
+
+   
+    //public void SetFillType(Image.FillMethod type)
+    //{
+    //    fillMethod = type;
+    //    Init(imagesAmount);
+    //}
+
+   
+}

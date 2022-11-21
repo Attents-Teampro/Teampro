81fa165f62ddf124ff6b6011b5294f08462c0ff9
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-11-21 15:20:29 +0900

    Revert "Revert "배경""
    This reverts commit b8cff35e7f3b9d0e727537671a28adbf4219b714.

--- Temp1/Assets/Scripts/Son/Objects/Break_Ghost.cs
+++ Temp1/Assets/Scripts/Son/Objects/Break_Ghost.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class Break_Ghost : MonoBehaviour
 {
+    // 벌룬 에셋에서 변형된 스크립트
+
     public bool Is_Breaked = false;
     public GameObject ghost_normal;
     public GameObject ghost_Parts;
     public Animator ghost;
     int counter;
-    // Start is called before the first frame update
+
+    public Transform player;
+    
+
     void Start()
     {
         ghost_normal.SetActive(true);
         ghost_Parts.SetActive(false);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        transform.LookAt(player);
         if(Is_Breaked == true)
         {
             ghost_Parts.SetActive(true);
@@ -26,6 +32,7 @@ public class Break_Ghost : MonoBehaviour
         }
         
     }
+
     public void break_Ghost()
     {
         Is_Breaked = true;

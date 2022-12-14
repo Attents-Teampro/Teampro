0ea206c4ba5454ee6a2657b1e24d8811bfcf6b48
jinseok0619 <chlwlstjr159@gmail.com>
2022-12-14 14:49:11 +0900

    Player 애니메이션

--- Temp1/Assets/Scripts/Player/AttackState.cs
+++ Temp1/Assets/Scripts/Player/AttackState.cs
@@ -14,26 +14,31 @@ public class AttackState : StateMachineBehaviour
 
     }
 
-        //override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
-        //{
-        //    Debug.Log("이펙트 꺼짐");
-        //    MainManager.instance.Player.WeaponEffectSwitch(false);  // 무기 이팩트 끄기
-
-        //    animator.ResetTrigger("doSwing");        // 어택 트리거도 일단 초기화
-        //}
-
-        //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        //{
-        //    Debug.Log("이펙트 켜짐");
-        //    MainManager.instance.Player.WeaponEffectSwitch(true);
-        //}
-
-        //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        //{
-        //    Debug.Log("이펙트 꺼짐");
-        //    MainManager.instance.Player.WeaponEffectSwitch(false);
-
-        //    animator.ResetTrigger("doSwing");        // 어택 트리거도 일단 초기화
-        //}
-
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Implement code that processes and affects root motion
     }
+
+    //override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+    //{
+    //    Debug.Log("이펙트 꺼짐");
+    //    MainManager.instance.Player.WeaponEffectSwitch(false);  // 무기 이팩트 끄기
+
+    //    animator.ResetTrigger("doSwing");        // 어택 트리거도 일단 초기화
+    //}
+
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    Debug.Log("이펙트 켜짐");
+    //    MainManager.instance.Player.WeaponEffectSwitch(true);
+    //}
+
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    Debug.Log("이펙트 꺼짐");
+    //    MainManager.instance.Player.WeaponEffectSwitch(false);
+
+    //    animator.ResetTrigger("doSwing");        // 어택 트리거도 일단 초기화
+    //}
+
+}

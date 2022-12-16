e806563eeeec981de504e41a232a3a7dfe52ce56
parkinyeop <parkinyeop@naver.com>
2022-12-16 14:31:38 +0900

    Coin 획득 사운드
    현재 소리 안남

--- Temp1/Assets/Scripts/Item/CoinDestroy.cs
+++ Temp1/Assets/Scripts/Item/CoinDestroy.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class CoinDestroy : MonoBehaviour
 {
     public int coinValue;
+    public AudioClip sfx_Coin;
+    
+    AudioSource audioSource;
 
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.PlayOneShot(sfx_Coin);
+            audioSource.Play();
             MainManager.instance.gold += coinValue;
             Destroy(this.gameObject);
             Debug.Log($"현재 플레이어 골드 : {MainManager.instance.gold}");
